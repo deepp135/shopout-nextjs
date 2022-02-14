4dc384b1852169cdf357a4ef535668fb4f51f1d0
Rajan <rajanvasani9@gmail.com>
2022-02-15 01:15:47 +0530

    updated api base url

--- src/configs/app.config.js
+++ src/configs/app.config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:5000/';
+export const BASE_URL = 'http://3.109.217.120:5001/';
 export const REACT_APP_TWO_FACTOR_KEY = '0e2f899e-2cb4-11eb-83d4-0200cd936042';
 
 export const MAX_PARTICIPANTS = 4;
