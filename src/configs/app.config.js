43fe0e96010893f26516c51baad8a2e88e6d14a0
Rajan <rajanvasani9@gmail.com>
2022-02-19 12:10:46 +0530

    api endpoint changed

--- src/configs/app.config.js
+++ src/configs/app.config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://3.109.217.120:5001/';
+export const BASE_URL = 'https://new-api.shopout.co.in/';
 export const REACT_APP_TWO_FACTOR_KEY = '0e2f899e-2cb4-11eb-83d4-0200cd936042';
 
 export const MAX_PARTICIPANTS = 4;
