f4b8ab4785fbe21db97b5afb1bb3024f68290510
Rajan <rajanvasani9@gmail.com>
2022-02-21 04:37:18 +0530

    set seller on big screen changes done

--- src/components/Video.js
+++ src/components/Video.js
@@ -2,21 +2,37 @@ import { AgoraVideoPlayer } from 'agora-rtc-react';
 import styles from '../styles/videoPart.module.css';
 
 const Videos = ({ users, tracks }) => {
+	let mainUser = users.find(user => user.uid === 1)
 	return (
 		<div className={styles['chat-part']}>
 			<div className={styles['video-chat']}>
 				{/* video-box */}
 				<div className={styles['video-box']}>
 					<div className={styles['main-image']}>
+					{mainUser ? 
+						<AgoraVideoPlayer
+							style={{ height: '100%', width: '100%' }}
+							videoTrack={mainUser.videoTrack}
+						/> 
+					: 
 						<AgoraVideoPlayer
 							style={{ height: '100%', width: '100%' }}
 							videoTrack={tracks[1]}
 						/>
+					}
 					</div>
 					<div className={styles['group-call']}>
 						<div className={styles['member-images']}>
+							{mainUser && 
+								<div className={styles.box}>
+									<AgoraVideoPlayer
+										style={{ height: '100%', width: '100%' }}
+										videoTrack={tracks[1] /*user.videoTrack*/}
+									/>
+								</div>
+							}
 							{users.length > 0 &&
-								users.map((user) => {
+								users.filter(user => user.uid !== 1).map((user) => {
 									if (user.videoTrack) {
 										return (
 											<div className={styles.box}>
