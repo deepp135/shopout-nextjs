71b39cc225ab901554dbfbbad9a09070b17ff866
Rajan <rajanvasani9@gmail.com>
2022-02-23 20:10:45 +0530

    error page style changes

--- src/pages/appointmentnotfound.js
+++ src/pages/appointmentnotfound.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import styles from '../styles/error.module.css';
 
 function AppointmentNotFound() {
-	return <div>The appointment is either expired or not available for this moment</div>;
+	return <div className={styles['container']}>The appointment is either expired or not available for this moment</div>;
 }
 
 export default AppointmentNotFound;
---
+++ src/pages/error.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../styles/error.module.css';
+
+function Error() {
+	return <div className={styles['container']}>You can not join this call at the moment</div>;
+}
+
+export default Error;
